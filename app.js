b6fa635b0115706e0aeff0f6747aef8efbf2aa25
MistaKitty <tiago.abreu.laibacas@gmail.com>
2024-06-22 10:38:25 +0100

    Lab started

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.use('/', index);
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
-module.exports = app;
+module.exports = app; 
 
